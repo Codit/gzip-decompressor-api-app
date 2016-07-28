1a8d5ce349550a08261ba1530703ced4c281bd88
Tom Kerkhove <kerkhove.tom@gmail.com>
2016-07-28 20:27:57 +0200

    Basic Validation
    Add basic validation on the input parameters

--- src/TomKerkhove.ApiApps.Decompressor/Controllers/DecompressionController.cs
+++ src/TomKerkhove.ApiApps.Decompressor/Controllers/DecompressionController.cs
@@ -35,6 +35,12 @@ namespace TomKerkhove.ApiApps.Decompressor.Controllers
         {
             try
             {
+                var validationResult = ValidateInput(uri, storageAccountName, storageAccountKey, containerName, blobName);
+                if (validationResult != null)
+                {
+                    return validationResult;
+                }
+
                 // Download the file
                 var compressedStream = await DownloadFileAsync(uri);
                 if (compressedStream == Stream.Null)
@@ -55,6 +61,69 @@ namespace TomKerkhove.ApiApps.Decompressor.Controllers
             {
                 return InternalServerError(ex);
             }
+        }
+
+        private IHttpActionResult ValidateInput(string uri, string storageAccountName, string storageAccountKey, string containerName, string blobName)
+        {
+            if (string.IsNullOrWhiteSpace(uri))
+            {
+                return BadRequest($"Invalid {nameof(uri)}.");
+            }
+
+            /*
+             * TODO: Validate storage account name against:
+             * 1. Account names must be from 3 through 24 characters long
+             * 2. Account names must alphanumeric
+             * Source: https://blogs.msdn.microsoft.com/jmstall/2014/06/12/azure-storage-naming-rules/
+             */
+
+            if (string.IsNullOrWhiteSpace(storageAccountName))
+            {
+                return BadRequest($"Invalid {nameof(storageAccountName)}.");
+            }
+
+            if (string.IsNullOrWhiteSpace(storageAccountKey))
+            {
+                return BadRequest($"Invalid {nameof(storageAccountKey)}.");
+            }
+
+            /*
+             * TODO: Validate container name against:
+             * 1. Container names must start with a letter or number, and can contain only letters, numbers, and the dash (-) character.
+             * 2. Every dash (-) character must be immediately preceded and followed by a letter or number; consecutive dashes are not permitted in container names.
+             * 3. All letters in a container name must be lowercase.
+             * 4. Container names must be from 3 through 63 characters long.
+             */
+
+            if (string.IsNullOrWhiteSpace(containerName))
+            {
+                return BadRequest($"Invalid {nameof(containerName)}.");
+            }
+
+            if (containerName.Length < 3)
+            {
+                return BadRequest($"Container name '{containerName}' is too short.");
+            }
+
+            if (containerName.Length > 63)
+            {
+                return BadRequest($"Container name '{containerName}' is too long.");
+            }
+
+            /*
+             * TODO: Validate blob name against:
+             * 1. A blob name can contain any combination of characters.
+             * 2. A blob name must be at least one character long and cannot be more than 1,024 characters long.
+             * 3. Blob names are case-sensitive.
+             * 4. Reserved URL characters must be properly escaped.
+             * 5. The number of path segments comprising the blob name cannot exceed 254. A path segment is the string between consecutive delimiter characters (e.g., the forward slash '/') that corresponds to the name of a virtual directory.
+             */
+            if (string.IsNullOrWhiteSpace(blobName))
+            {
+                return BadRequest($"Invalid {nameof(blobName)}.");
+            }
+
+            return null;
         }
 
         private static async Task<Stream> DownloadFileAsync(string uri)
@@ -101,7 +170,7 @@ namespace TomKerkhove.ApiApps.Decompressor.Controllers
             var blobClient = account.CreateCloudBlobClient();
 
             // Get a reference to the container
-            var container = blobClient.GetContainerReference(containerName);
+            var container = blobClient.GetContainerReference(containerName.ToLower());
             await container.CreateIfNotExistsAsync();
 
             // Get a reference to the blob
@@ -113,3 +182,4 @@ namespace TomKerkhove.ApiApps.Decompressor.Controllers
         }
     }
 }
+
