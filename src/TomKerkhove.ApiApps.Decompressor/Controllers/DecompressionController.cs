9a3244956c5d80d0fe82571acd6a41746898c3ae
Tom Kerkhove <kerkhove.tom@gmail.com>
2016-07-28 20:16:07 +0200

    Basic Exception Handling
    Add basic exception handling

--- src/TomKerkhove.ApiApps.Decompressor/Controllers/DecompressionController.cs
+++ src/TomKerkhove.ApiApps.Decompressor/Controllers/DecompressionController.cs
@@ -1,10 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.Compression;
 using System.Net.Http;
 using System.Web.Http;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
+using Swashbuckle.Swagger.Annotations;
+using TomKerkhove.ApiApps.Decompressor.Exceptions;
 
 namespace TomKerkhove.ApiApps.Decompressor.Controllers
 {
@@ -25,17 +28,33 @@ namespace TomKerkhove.ApiApps.Decompressor.Controllers
         /// <param name="containerName">Name of the container</param>
         /// <param name="blobName">Name of the blob</param>
         /// <response code="200">The file was successfully processed</response>
+        /// <response code="500">We were unable to process the file</response>
         [HttpGet]
         public async Task<IHttpActionResult> Get(string uri, string storageAccountName, string storageAccountKey,
             string containerName, string blobName)
         {
-            var compressedStream = await DownloadFileAsync(uri);
+            try
+            {
+                // Download the file
+                var compressedStream = await DownloadFileAsync(uri);
+                if (compressedStream == Stream.Null)
+                {
+                    return NotFound();
+                }
 
-            var unzippedStream = await UnzipStream(compressedStream);
+                // Decompress the stream
+                var uncompressedStream = await DecompressStreamAsync(compressedStream);
 
-            await StoreStream(storageAccountName, storageAccountKey, containerName, blobName, unzippedStream);
+                await
+                    StoreOnBlobStorageAsync(storageAccountName, storageAccountKey, containerName, blobName,
+                        uncompressedStream);
 
-            return Ok();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
 
         private static async Task<Stream> DownloadFileAsync(string uri)
@@ -43,34 +62,54 @@ namespace TomKerkhove.ApiApps.Decompressor.Controllers
             var httpClient = new HttpClient();
             var response = await httpClient.GetAsync(uri);
 
-            var responseStream = await response.Content.ReadAsStreamAsync();
-            return responseStream;
+            return (response.IsSuccessStatusCode) ? await response.Content.ReadAsStreamAsync() : Stream.Null;
         }
 
-        private static async Task<Stream> UnzipStream(Stream responseStream)
+        private static async Task<Stream> DecompressStreamAsync(Stream compressedStream)
         {
-            var unzippedStream = new MemoryStream();
-            using (var gzipStream = new GZipStream(responseStream, CompressionMode.Decompress))
+            try
             {
-                await gzipStream.CopyToAsync(unzippedStream);
-                unzippedStream.Seek(0, SeekOrigin.Begin);
+                // Seek to origin, if possible
+                if (compressedStream.CanSeek && compressedStream.Position > 0)
+                {
+                    compressedStream.Seek(0, SeekOrigin.Begin);
+                }
+
+                var unzippedStream = new MemoryStream();
+                using (var gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+                {
+                    await gzipStream.CopyToAsync(unzippedStream);
+
+                    // Seek back to the origin of the unzipped stream
+                    unzippedStream.Seek(0, SeekOrigin.Begin);
+                }
+
+                return unzippedStream;
             }
-            return unzippedStream;
+            catch (Exception ex)
+            {
+                // Wrap in a custom exception to clearly state that it's the decompression that failed
+                throw new DecompressionException("Failed to decompress the stream", ex);
+            }
         }
 
-        private async Task StoreStream(string storageAccountName, string storageAccountKey, string containerName, string blobName, Stream unzippedStream)
+        private async Task StoreOnBlobStorageAsync(string storageAccountName, string storageAccountKey, string containerName, string blobName, Stream decompressedStream)
         {
+            // Create client
             StorageCredentials storageCreds = new StorageCredentials(storageAccountName, storageAccountKey);
             CloudStorageAccount account = new CloudStorageAccount(storageCreds, useHttps: true);
             var blobClient = account.CreateCloudBlobClient();
 
+            // Get a reference to the container
             var container = blobClient.GetContainerReference(containerName);
             await container.CreateIfNotExistsAsync();
 
+            // Get a reference to the blob
             var blob = container.GetBlockBlobReference(blobName);
-            await blob.UploadFromStreamAsync(unzippedStream);
+            await blob.UploadFromStreamAsync(decompressedStream);
 
-            unzippedStream.Dispose();
+            // Cleanup
+            decompressedStream.Dispose();
         }
     }
 }
---
+++ src/TomKerkhove.ApiApps.Decompressor/Exceptions/DecompressionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TomKerkhove.ApiApps.Decompressor.Exceptions
+{
+    [System.Serializable]
+    public class DecompressionException : Exception
+    {
+        public DecompressionException() { }
+        public DecompressionException(string message) : base(message) { }
+        public DecompressionException(string message, Exception inner) : base(message, inner) { }
+        protected DecompressionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
