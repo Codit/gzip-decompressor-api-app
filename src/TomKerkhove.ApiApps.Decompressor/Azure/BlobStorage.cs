d9e789dc79eca92997138f53cd46e97b20df8f34
Tom Kerkhove <kerkhove.tom@gmail.com>
2016-07-29 17:48:25 +0200

    Gzip & Blob Storage tooling
    Add seperate classes for Gzip & Blob Storage tooling

---
+++ src/TomKerkhove.ApiApps.Decompressor/Azure/BlobStorage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace TomKerkhove.ApiApps.Decompressor.Azure
+{
+    /// <summary>
+    /// Azure Blob Storage library
+    /// </summary>
+    public static class BlobStorage
+    {
+        /// <summary>
+        /// Uploads a data to Blob Storage
+        /// </summary>
+        /// <param name="storageAccountName">Name of the storage account</param>
+        /// <param name="storageAccountKey">Key to access the storage account</param>
+        /// <param name="containerName">Name of the destionation container</param>
+        /// <param name="blobName">Name of the blob</param>
+        /// <param name="contentStream">Stream containing the blob contents</param>
+        public static async Task UploadAsync(string storageAccountName, string storageAccountKey, string containerName, string blobName, Stream contentStream)
+        {
+            if (contentStream.CanRead == false)
+            {
+                throw new ArgumentException("Contents stream is not readable.");
+            }
+
+            if (contentStream.Position > 0&& contentStream.CanSeek)
+            {
+                contentStream.Seek(0, SeekOrigin.Begin);
+            }
+
+            // Create client
+            StorageCredentials storageCreds = new StorageCredentials(storageAccountName, storageAccountKey);
+            CloudStorageAccount account = new CloudStorageAccount(storageCreds, useHttps: true);
+            var blobClient = account.CreateCloudBlobClient();
+
+            // Get a reference to the container
+            var container = blobClient.GetContainerReference(containerName.ToLower());
+            await container.CreateIfNotExistsAsync();
+
+            // Get a reference to the blob
+            var blob = container.GetBlockBlobReference(blobName);
+            await blob.UploadFromStreamAsync(contentStream);
+        }
+    }
+}
---
+++ src/TomKerkhove.ApiApps.Decompressor/Compression/Gzip.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using TomKerkhove.ApiApps.Decompressor.Exceptions;
+
+namespace TomKerkhove.ApiApps.Decompressor.Compression
+{
+    /// <summary>
+    /// Gzip compression library
+    /// </summary>
+    public static class Gzip
+    {
+        /// <summary>
+        /// Decompresses a stream
+        /// </summary>
+        /// <param name="compressedStream">Compressed stream in GZIP format</param>
+        /// <param name="destionationStream">Destionation stream that will contain decompressed data</param>
+        public static async Task DecompressToAsync(Stream compressedStream, Stream destionationStream)
+        {
+            try
+            {
+                // Seek to origin, if possible
+                if (compressedStream.CanSeek && compressedStream.Position > 0)
+                {
+                    compressedStream.Seek(0, SeekOrigin.Begin);
+                }
+                
+                using (var gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+                {
+                    await gzipStream.CopyToAsync(destionationStream);
+
+                    // Seek back to the origin of the unzipped stream
+                    destionationStream.Seek(0, SeekOrigin.Begin);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Wrap in a custom exception to clearly state that it's the decompression that failed
+                throw new DecompressionException("Failed to decompress the stream", ex);
+            }
+        }
+    }
+}
